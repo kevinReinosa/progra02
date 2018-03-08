1d1cbc412a46212cef872d8639fc89a035270f14
user <user@com2pc22>
2018-03-08 04:47:21 -0600

    Crear un hola mundo

---
+++ src/hola_mundo/Hola_mundo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hola_mundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author user
+ */
+public class Hola_mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null,"HOLA MUNDO EN GIT");
+        
+    }
+    
+}
